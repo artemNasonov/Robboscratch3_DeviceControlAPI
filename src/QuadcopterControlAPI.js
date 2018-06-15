9d39d9382d541495ef7b3a0acc5f5a585efabd3f
Yarila <yarila682@yandex.ru>
2018-06-15 16:09:37 +0300

    Quadcopter control api

---
+++ src/QuadcopterControlAPI.js
@@ -0,0 +1,12 @@
+import DeviceControlAPI from './DeviceControlAPI';
+//import RobotSensorsData from './RobotSensorsData';
+import {InterfaceDevice,searchDevices,getConnectedDevices,pushConnectedDevices,DEVICES,DEVICE_STATES} from './chrome';
+
+
+export default class QuadcopterControlAPI extends DeviceControlAPI {
+
+
+   
+
+
+}
--- src/Robboscratch3_DeviceControlAPI.js
+++ src/Robboscratch3_DeviceControlAPI.js
@@ -2,13 +2,16 @@
 
 import RobotControlAPI from './RobotControlAPI';
 import LaboratoryControlAPI from './LaboratoryControlAPI';
+import QuadcopterControlAPI from './QuadcopterControlAPI';
 import DeviceControlAPI from './DeviceControlAPI';
+
 
 
 export  {
 
   RobotControlAPI,
   LaboratoryControlAPI,
+  QuadcopterControlAPI,
   DeviceControlAPI
 
 };
